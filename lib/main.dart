9ecb95756c6f7986ba3862de1a1600fd4f3a46c4
GeronimoB <geornimobustos@gmail.com>
2024-02-16 23:50:19 -0500

    Intro

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,67 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:bro_app_to/Intro.dart';
+import 'package:bro_app_to/Intro.dart'; // Asegúrate de importar correctamente tu archivo Intro.dart
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const MySplashScreen(), // Cambio a la pantalla de introducción
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => SignInPage()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => SignInPage()),
-            );
-          },
-          
-          child: const Text('Go to Sign In Page'),
+      backgroundColor: Colors.black, // Fondo negro
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            colors: [
+              Colors.black, // Color negro en la parte inferior
+              Colors.grey[900]!, // Color negro un poco más claro arriba
+            ],
+          ),
+        ),
+        child: Center(
+          child: Image.asset(
+            'assets/Logo.png', // Nombre de la imagen del logo
+            width: 246, // Ancho del logo
+            height: 117, // Alto del logo
+          ),
         ),
       ),
     );
