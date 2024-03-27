3136d5a01676fc4a92aa4b075ef272664ac0e009
GeronimoB <geronimobustos@gmail.com>
2024-03-27 07:52:53 -0500

    add details plan

---
+++ lib/DetallesPlan.dart
@@ -0,0 +1,141 @@
+import 'package:bro_app_to/Screens/metodo_pago_screen.dart';
+import 'package:bro_app_to/components/custom_text_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+// Define el widget de detalles de planes
+class PlanDetailsScreen extends StatelessWidget {
+  // Recibe los parámetros desde la pantalla anterior
+  final String goldPlan;
+  final List<String> planItems;
+  final String price;
+
+  // Constructor que asigna los parámetros
+  PlanDetailsScreen({
+    required this.goldPlan,
+    required this.planItems,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'PLANES',
+              style: TextStyle(
+                fontFamily: 'Montserrat',
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SvgPicture.asset(
+                'assets/icons/Logo.svg',
+                width: 30, // Ancho de 30
+                height: 30, // Alto de 30
+              ),
+                Text(
+                  goldPlan,
+                  style: const TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 20,
+                    color: Color(0xff00E050),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            // Espacio
+            SizedBox(height: 20),
+            // Lista de elementos del plan
+            ...generatePlanItems(planItems),
+            // Descripción en latín
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                style: TextStyle(fontFamily: 'Montserrat'),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            // Espacio
+            SizedBox(height: 20),
+            // Precios y botón de subscripción en dos columnas
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                // Columna del precio
+                Column(
+                  children: <Widget>[
+                    const Text(
+                      '1 MES',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        color: Color(0xff00E050),
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      price,
+                      style: const TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                CustomTextButton(
+                  onTap: () {
+                    final String priceString = price.replaceAll(',', '.');
+                    final double? priceDouble = double.tryParse(priceString);
+                    if (priceDouble != null) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MetodoDePagoScreen(
+                            valueToPay: priceDouble,
+                          ),
+                        ),
+                      );
+                    } else {
+                      print('El precio no es un número válido.');
+                    }
+                  },
+                  text: ' Subscribirse',
+                  buttonPrimary: true,
+                  width: 116,
+                  height: 39,
+                ),
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+List<Widget> generatePlanItems(List<String> items) {
+  return items.map((item) {
+    return ListTile(
+      title: Text(
+        item,
+        style: TextStyle(fontFamily: 'Montserrat', color: Colors.white),
+      ),
+      leading: Icon(Icons.check_circle, color: Color(0xff00E050)), // Icono con check verde
+    );
+  }).toList();
+}
+
+}
--- lib/planes_pago.dart
+++ lib/planes_pago.dart
@@ -1,3 +1,4 @@
+import 'package:bro_app_to/DetallesPlan.dart';
 import 'package:bro_app_to/components/custom_box_shadow.dart';
 import 'package:bro_app_to/components/custom_text_button.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,13 @@ List<Plan> planes = [
         'Posibilita subir 2 videos de un máximo de 2min cada uno de ellos, con una calidad recomendada de 1080 Full HD.',
     descripcionLarga:
         'Suscripcion Basic: PVP 19,99€. Tiempo de vigencia un mes. Posibilita subir 2 videos (3 videos) de un máximo de 2min cada uno de ellos, con una calidad recomendada de 1080 Full HD. Sube nuevo video una vez tengas el cupo de 2 cubierto, para ello deberás eliminar uno de la plataforma para que sea efectiva la subida, y seguir manteniendo tus 2 videos (3 videos) activos. Destaca tu video: PVP 2,99€ por video (Recomendación 0,99€ por video).',
+    cualidades: [
+      "Subida de 5 videos",
+      "Duración máxima de 2 minutos por video",
+      "Calidad recomendada de 1080 Full HD",
+      "Posibilidad de subir un nuevo video al cubrir el cupo eliminando uno previo",
+      "Mantenimiento de 5 videos activos",
+    ],
   ),
   Plan(
     nombre: 'Gold',
@@ -28,6 +36,13 @@ List<Plan> planes = [
         'Posibilita subir 5 videos de un máximo de 2min cada uno de ellos, con una calidad recomendada de 1080 Full HD.',
     descripcionLarga:
         'Sube nuevo video una vez tengas el cupo de 5 cubierto, para ello deberás eliminar uno de la plataforma para que sea efectiva la subida y seguir manteniendo tus 5 videos activos..',
+    cualidades:[ "Subida de 5 videos",
+      "Duración máxima de 2 minutos por video",
+      "Calidad recomendada de 1080 Full HD",
+      "Posibilidad de subir un nuevo video al cubrir el cupo eliminando uno previo",
+      "Mantenimiento de 5 videos activos."
+      ],
+
   ),
   Plan(
     idPlan: 'price_1Owcf5IkdX2ffauudm4v0KAQ',
@@ -37,6 +52,12 @@ List<Plan> planes = [
         'Posibilita subir 10 videos de un máximo de 2min cada uno de ellos, con una calidad recomendada de 1080 Full HD.',
     descripcionLarga:
         'Sube nuevo video una vez tengas el cupo de 10 cubiertos, para ellos deberás eliminar uno de la plataforma para que sea efectiva la subida y seguir  manteniendo tus 10 videos activos',
+      cualidades: ["Subida de 10 videos", 
+      "Duración máxima de 2 minutos por video", 
+      "Calidad recomendada de 1080 Full HD", 
+      "Posibilidad de subir un nuevo video al cubrir el cupo eliminando uno previo",
+       "Mantenimiento de 10 videos activos."],
+
   ),
   Plan(
     idPlan: 'price_1OwcfMIkdX2ffauuEqdLsX5h',
@@ -46,6 +67,11 @@ List<Plan> planes = [
         'Dicha suscripción tiene una duración de 12 meses, y posibilita subir videos ilimitados con un máximo de 2min',
     descripcionLarga:
         'Dicha suscripción tiene una duración de 12 meses, y posibilita subir videos ilimitados con un máximo de 2min cada uno de ellos, con una calidad recomendada de 1080 Full HD',
+      cualidades:["Suscripción de 12 meses",
+       "Subida ilimitada de videos",
+        "Duración máxima de 2 minutos por video",
+         "Calidad recomendada de 1080 Full HD."],
+
   ),
 ];
 
@@ -104,27 +130,30 @@ class _PlanesPagoState extends State<PlanesPago> {
                 Padding(
                   padding: const EdgeInsets.all(22.0),
                   child: CustomTextButton(
-                      onTap: () {
-                        final playerProvider =
-                            Provider.of<PlayerProvider>(context, listen: false);
-                        final precio = playerProvider
-                            .getActualPlan()!
-                            .precio
-                            .replaceAll(',', '.');
-
+                    onTap: () {
+                      final playerProvider = Provider.of<PlayerProvider>(context, listen: false);
+                      final planActual = playerProvider.getActualPlan();
+                      
+                      if (planActual != null) {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => MetodoDePagoScreen(
-                                    valueToPay: precio as double,
-                                  )),
+                            builder: (context) => PlanDetailsScreen(
+                              goldPlan: planActual.nombre,
+                              price: planActual.precio,
+                              planItems: planActual.cualidades,
+                            ),
+                          ),
                         );
-                      },
-                      text: 'Siguiente',
-                      buttonPrimary: true,
-                      width: 116,
-                      height: 39),
+                      }
+                    },
+                    text: 'Siguiente',
+                    buttonPrimary: true,
+                    width: 116,
+                    height: 39,
+                  ),
                 ),
+
                 Align(
                   alignment: Alignment.bottomCenter,
                   child: Padding(
