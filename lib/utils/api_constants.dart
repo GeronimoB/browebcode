639c72aa60a5b353952889135f3652efe26ff1cb
Alejandro Padilla <alejopadilla297@gmail.com>
2024-03-31 20:02:20 -0500

    .

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.1.108:8080';
+  //static const String baseUrl = 'http://192.168.1.108:8080';
 
-  //static const String baseUrl = 'https://back.xn--br-6ja.com';
+  static const String baseUrl = 'https://back.xn--br-6ja.com';
   static const String stripePublicKey =
       'pk_test_51OHUSwIkdX2ffauuyXaqeAemhHoR58QXrZAlX8bIrUZuix4E0zhlARxzPBCGsT7dE4KymH23wtTXn7JO9DDoTNC5002jmBsZCu';
 }
